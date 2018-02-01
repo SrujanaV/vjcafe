c33c59c13cfa321f70221a0722f2acd11e584e20
Srujana <srujana.vemula@wohlig.com>
2018-02-01 14:02:06 +0530

    store locator

--- frontend/views/content/storelocator/storelocator.js
+++ frontend/views/content/storelocator/storelocator.js
@@ -4,9 +4,63 @@ myApp.controller('StorelocatorCtrl', function ($scope, TemplateService, Navigati
         $scope.navigation = NavigationService.getNavigation();
 
         $scope.locations=[{
-            "name":"Head Office",
-            "address1":"Bloombay Enterprises LLP, Deividayal Mill Compound,",
-            "address2":"Reay Road, Darukhana, ",
-            "address3":"Mumbai, India 400010"
+            "name":"Sion West",
+            "address":"<b>17/A, Shashi building,ground floor,<br>sion circle, sion west.<br>Mumbai - 400022<b>",
+            "phone":"23425"
+        },{
+            "name":"Vile Parle West",
+            "address":"Shop 2, Ground Floor, Shree Nataraj Building,<br>Opposite Bhaidas Auditorium, JVPD Lane,<br> Vile Parle West, Mumbai<br>Maharashtra 400056",
+            "phone":"02269999074"
+        },{
+            "name":"Mulund West",
+            "address":"",
+            "phone":""
+        },{
+            "name":"Borivali West",
+            "address":"",
+            "phone":""
+        },{
+            "name":"Chembur",
+            "address":"",
+            "phone":""
+        },{
+            "name":"Ghatkopar East",
+            "address":"",
+            "phone":""
+        },{
+            "name":"Mira Road",
+            "address":"",
+            "phone":""
+        },{
+            "name":"Matunga East",
+            "address":"",
+            "phone":""
+        },{
+            "name":"Bandra West",
+            "address":"",
+            "phone":""
+        },{
+            "name":"Kandivali West",
+            "address":"",
+            "phone":""
+        },{
+            "name":"Malad West",
+            "address":"",
+            "phone":""
+        },{
+            "name":"Thane West",
+            "address":"",
+            "phone":""
+        },{
+            "name":"Bhayandar",
+            "address":"",
+            "phone":""
         }]
+
+        $scope.changePlaces=function(location){
+            $scope.locationDropdownOpen = false;
+            $scope.locationButton = location.name; // After clicking on dropdown, the button value changes
+            $scope.showSlider = false;
+            $scope.storeLocation = [];
+        }
     })
