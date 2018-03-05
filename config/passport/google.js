9964c997c1f7d953f3d8695cc07b76194e44d84d
prajakta <prajakta.kamble@wohlig.com>
2018-03-05 15:46:32 +0530

    google login enabled

--- config/passport/google.js
+++ config/passport/google.js
@@ -1,6 +1,10 @@
+// global["GoogleKey"] = "AIzaSyDMgwXi6D38isibUfShc9C2mJyaHZZ2LpE";
+// global["GoogleclientId"] = "529279279497-hdm2ul03erq4kitk7qlqbf41h6pl8f7p.apps.googleusercontent.com";
+// global["GoogleclientSecret"] = "nNZiqXW5U2364QI9--sVIR8B";
 global["GoogleKey"] = "AIzaSyDMgwXi6D38isibUfShc9C2mJyaHZZ2LpE";
-global["GoogleclientId"] = "529279279497-hdm2ul03erq4kitk7qlqbf41h6pl8f7p.apps.googleusercontent.com";
-global["GoogleclientSecret"] = "nNZiqXW5U2364QI9--sVIR8B";
+global["GoogleclientId"] = "1066475903870-qhd7d3htvemhepouhg0kl9p1qgidvfue.apps.googleusercontent.com";
+global["GoogleclientSecret"] = "NYT1_QLs8YWireRBP2iQZF9W";
+
 
 passport.use(new GoogleStrategy({
         clientId: GoogleclientId,
